55c3a5f6961ffedeeae691ad5bf7ec2c68182d5d
lizzysus <yamanshrestha2027@gmail.com>
2026-08-07 18:53:34 +0530

    Movement Code Added

--- My project/Assets/Codes/Player.cs
+++ My project/Assets/Codes/Player.cs
@@ -3,17 +3,25 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [Header("Movement")]
-    [SerializeField] private float charInp;
     [SerializeField] private float movSpeed;
+    [SerializeField] private Rigidbody2D rb;
+    private float charInp;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
-
+        HandleMovement();
     }
 
     private void HandleMovement() 
     {
         charInp = (Input.GetAxisRaw("Horizontal"));
 
+        rb.linearVelocity = new Vector2(charInp * movSpeed, rb.linearVelocity.y);
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
 }
