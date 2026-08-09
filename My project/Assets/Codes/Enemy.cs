0223afe45249a24163e40279a3f612fa5f71be98
lizzysus <yamanshrestha2027@gmail.com>
2026-08-09 17:48:20 +0530

    Update Enemy.cs

--- My project/Assets/Codes/Enemy.cs
+++ My project/Assets/Codes/Enemy.cs
@@ -3,16 +3,17 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
+    private SpriteRenderer sr;
+
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
-
-
-    // Update is called once per frame
     void Update()
     {
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
+
 }
